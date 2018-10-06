ee17f3b2e92132f88a36c40b7e1c7d0f4ada4604
tamara0715@gmail.com <tamara0715@gmail.com>
2018-10-06 02:22:39 -0500

    Start work on Module 1 of the course

---
+++ EdXModules/Module1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            /*
+             * Casting:
+             * Two types of conversion: implicit and explicit
+             * 
+             * Implicit
+             *  int myInt = 214748647;
+             *  long myLong = myInt;
+             *  
+             * Explicit
+             *  double myDouble = 1234.6;
+             *  int myInt;
+             *  myInt = (int)myDouble;
+             *  
+             * Explicit 2 
+             *  double myDouble = 1234.6;
+             *  int myInt;
+             *  myInt = Convert.ToInt32(myDouble);
+             *  
+             * TryParse()
+             *  bool result = Int32.TryParse(value, out number);
+             *   
+             * Parse()
+             *  int number = Int32.Parse(value);
+             */
+        }
+    }
+}
